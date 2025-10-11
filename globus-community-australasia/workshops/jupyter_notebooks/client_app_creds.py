ddf9e16e492a4e7958967eff4c893454181d43da
alex-ip <alex.ip@aarnet.edu.au>
2025-10-11 23:58:23 +0000

    Jupyter notebooks updated

--- globus-community-australasia/workshops/jupyter_notebooks/client_app_creds.py
+++
@@ -1,3 +0,0 @@
-# Service user ID & secret for <client_app_uuid>@clients.auth.globus.org
-CLIENT_ID = "<client_app_uuid>" # This ID must be authorised to write to the destination collection
-CLIENT_SECRET = "<client_app_secret>" # This is bad practice - do NOT hard-code secrets
